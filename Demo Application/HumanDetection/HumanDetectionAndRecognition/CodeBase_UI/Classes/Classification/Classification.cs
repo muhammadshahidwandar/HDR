d34cf242f31a08aa0f67968ecdb9eab842c7dff7
Muhammad Shahid <shahid.eyecom@gmail.com>
2016-11-16 16:23:08 +0500

    Demo Application
    Successfully classified for human and non human objects

---
+++ Demo Application/HumanDetection/HumanDetectionAndRecognition/CodeBase_UI/Classes/Classification/Classification.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.Util;
+using System.Windows.Forms;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System.Drawing;
+using ZedGraph;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace HumanDetectionAndRecognition
+{
+    public class Classification
+    {
+        #region Public Variables
+        public List<List<int>> _vectorsFeature = new List<List<int>>();
+        public List<List<int>> _trainVF = new List<List<int>>();
+        public string vectorFeatureStore = "D:\\vectorFeatureStore.bin";
+        int Threshold = 100;
+        #endregion
+        /// <summary>
+        /// This function take Connected Component and it only call another function Called ==> GetFeatureVector
+        /// to calculate VectorFeature for this Connected Component
+        /// and will Assign the result in public variable (_vectorsFeature)
+        /// </summary>
+        /// <param name="image">Connected Components of training object</param>
+        public void Training_Image(Image<Gray, byte> image)
+        {
+            List<int> _vectorFeature;
+            _vectorFeature = GetFeatureVector(image);
+            #region Add Vector To Vectors
+            _vectorsFeature.Add(_vectorFeature);
+            #endregion
+        }
+        /// <summary>
+        /// This function take Connected Component for unknown object and
+        /// Threshold ToCompare with min disstortion of it and the trained image
+        /// </summary>
+        /// <param name="image">Connected Componets of object</param>
+        /// <param name="Threshold">Threshold if it > min diss therefore it's human</param>
+        /// <returns>true, if it is Human.
+        /// false, if else.</returns>
+        public bool Classify_Image(Image<Gray, byte> image, int Threshold)
+        {
+            #region Private Variables
+            List<int> _dist = new List<int>();
+            List<int> _vectorFeature = new List<int>();
+            int _minDis = 10000;
+            int _vectorDiss = 0;
+            #endregion
+
+            //Retrun in Public Variable _vectorsFeature
+            _vectorFeature = GetFeatureVector(image);
+
+            for (int i = 0; i < _trainVF.Count; i++)
+            {
+                for (int j = 0; j < 40; j++)
+                    {
+                        _vectorDiss += Math.Abs(_vectorFeature[j] - _trainVF[i][j]);
+                    }
+                    _dist.Add(_vectorDiss);
+                    _vectorDiss = 0;
+             }
+            _dist.Sort();
+            if (_dist.Count == 0)
+                return false;
+            _minDis = _dist[0];
+
+            if (_minDis < Threshold)
+            {
+                MessageBox.Show("It's Human");
+               // MessageBox.Show("It's Human");
+                return true;
+            }
+
+            MessageBox.Show("No it Isn't Human");
+            return false;
+        }
+        /// <summary>
+        /// This function calculate the VectorFeature for the given Connected Components
+        /// </summary>
+        /// <param name="_image">Connected Componets of object</param>
+        private List<int> GetFeatureVector(Image<Gray, byte> _image)
+        {
+            #region Private Variables
+            bool _next = true;
+            int _left_len = 0, _right_len = 0;
+            int CntLeft = 1, CntRight = 1;
+            Image<Gray, byte> image2;
+            //VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint(); //VectorOfPoint 
+            VectorOfPoint contours;
+            List<int> _vectorFeature;
+            List<Point> _vectorFeature_left;
+            List<Point> _vectorFeature_right;
+            #endregion
+            #region Get Contour Of Image
+            image2 = _image.Resize(40, 80, 0);  //20 ,40;
+            //   CvInvoke.Canny(image2,image2, 100, 200); 
+            //     Contour ctr = _image.FindContours(Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_NONE, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_CCOMP);
+            contours = Classification.FindLargestContour(image2, image2);
+            // CvInvoke.FindContours(image2, contours, null, Emgu.CV.CvEnum.RetrType.List, Emgu.CV.CvEnum.ChainApproxMethod.LinkRuns);
+            #endregion
+            #region Draw rectangle
+           image2.Draw(CvInvoke.BoundingRectangle(contours), new Gray(255), 1);
+            Point a = contours[0];
+            a.Y = a.Y - 9;
+            a.X = a.X - 40;
+
+            //Draw the label for each detected person
+            //ImgGray.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.LightGreen));
+            CvInvoke.PutText(
+                               image2,
+                                "    Person ",
+                                a,
+                                FontFace.HersheyComplex,
+                                0.3,
+                                new MCvScalar(255, 0, 0));
+            #endregion 
+            int total = contours.Size;
+            #region Initialization
+            _vectorFeature = new List<int>();
+            _vectorFeature_left = new List<Point>();
+            _vectorFeature_right = new List<Point>();
+
+            #endregion
+
+            if (contours == null)
+                return _vectorFeature;
+            //      while(_next)
+            //           for (int k = 0; k < total; k++)
+            //           {
+            if (contours.Size >= 40)
+            {
+                #region GetPixels
+                for (int k = 0; k < contours.Size; k++)
+                {
+
+                    //Left Hand Side
+                    if (contours[k].X <= 20)
+                        _vectorFeature_left.Add(contours[k]);
+                    //Right Hand Side
+                    else if (contours[k].X > 20)
+                        _vectorFeature_right.Add(contours[k]);
+                }
+                if (_vectorFeature_left.Count < 20 || _vectorFeature_right.Count < 20)
+                { }
+                #endregion
+                else
+                {
+                    #region Select 10 Points From Left and Right
+                    _left_len = _vectorFeature_left.Count;
+                    _right_len = _vectorFeature_right.Count;
+                    if (_left_len >= 40)
+                        CntLeft = _left_len / 20;
+                    if (_right_len >= 40)
+                        CntRight = _right_len / 20;
+                    //20 From Left
+                    for (int i = 0; i < _left_len; )
+                    {
+                        if (_vectorFeature.Count >= 20)
+                            break;
+                        _vectorFeature.Add(_vectorFeature_left[i].X);
+                        i += CntLeft;
+                    }
+                    //20 From Right
+                    for (int i = 0; i < _right_len; )
+                    {
+                        if (_vectorFeature.Count >= 40)
+                            break;
+                        _vectorFeature.Add(_vectorFeature_right[i].Y);
+                        i += CntRight;
+                    }
+                    #endregion
+                  //  #region Add Vector To Vectors
+                 //   _vectorsFeature.Add(_vectorFeature);
+                 //   #endregion
+                    return _vectorFeature;
+                }
+            }
+            //#region Check For Next Contour
+            //if (contours.HNext != null)
+            //    contours = contours.HNext;
+            //else
+            //    _next = false;
+            //#endregion
+            return _vectorFeature;
+        }
+
+
+        /// <summary>
+        /// Human Classification Code :)
+        /// Determine if the Connected component is Human or not.
+        /// </summary>
+        /// <param name="list">list of connected components to be classified</param>
+        /// <returns>list of Humans</returns>
+        public List<ComponentData> classify(List<ComponentData> list)
+        {
+            List<ComponentData> humans = new List<ComponentData>();
+            for (int i = 0; i < list.Count; i++)
+                if (Classify_Image(list[i].Silhouette, Threshold))
+                    humans.Add(list[i]);
+            return humans;
+        }
+
+        /// <summary>
+        /// Loading the FeatureVectors of the trained Connected Components
+        /// in public variable ( _trainVF )
+        /// </summary>
+        public void LoadFeatureVectors()
+        {
+            try
+            {
+                if (File.Exists(vectorFeatureStore))
+                {
+                    IFormatter formatter1 = new BinaryFormatter();
+                    Stream stream1 = new FileStream(vectorFeatureStore, FileMode.Open, FileAccess.Read, FileShare.Read);
+                    _trainVF = (List<List<int>>)formatter1.Deserialize(stream1);
+                    stream1.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Feature vector data does not exist!");
+                    Application.Exit();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
+        }
+        /// <summary>
+        /// Saving FeatureVectors of Training Connected Components
+        /// </summary>
+        public void SaveFeatureVectors()
+        {
+            Stream stream1;
+            try
+            {
+                IFormatter formatter1 = new BinaryFormatter();
+                if (File.Exists(vectorFeatureStore))
+                stream1 = new FileStream(vectorFeatureStore, FileMode.Open, FileAccess.Write, FileShare.None);
+                else
+                    stream1 = new FileStream(vectorFeatureStore, FileMode.Create, FileAccess.Write, FileShare.None);
+
+                formatter1.Serialize(stream1, _vectorsFeature);
+                stream1.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error In Saving File ...");
+            }
+        }
+
+        /// <summary>
+        /// Find Largest Contour 
+        /// </summary>
+
+        public static VectorOfPoint FindLargestContour(IInputOutputArray cannyEdges, IInputOutputArray result)
+        {
+            int largest_contour_index = 0;
+            double largest_area = 0;
+            VectorOfPoint largestContour;
+
+            using (Mat hierachy = new Mat())
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                IOutputArray hirarchy;
+
+                CvInvoke.FindContours(cannyEdges, contours, hierachy, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxNone);// RetrType.Tree, ChainApproxMethod.ChainApproxNone);
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    MCvScalar color = new MCvScalar(0, 0, 255);
+
+                    double a = CvInvoke.ContourArea(contours[i], false);  //  Find the area of contour
+                    if (a > largest_area)
+                    {
+                        largest_area = a;
+                        largest_contour_index = i;                //Store the index of largest contour
+                    }
+
+                    CvInvoke.DrawContours(result, contours, largest_contour_index, new MCvScalar(255, 0, 0), 5);
+                }
+
+                CvInvoke.DrawContours(result, contours, largest_contour_index, new MCvScalar(0, 0, 255), 1);//,LineType.EightConnected , hierachy);
+                largestContour = new VectorOfPoint(contours[largest_contour_index].ToArray());
+            }
+
+            return largestContour;
+        }
+
+    }
+}
+
+
---
+++ Demo Application/HumanDetection/HumanDetectionAndRecognition/CodeBase_UI/Classes/ConnectedComponent/ConnectedComponent.cs
@@ -0,0 +1,305 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Emgu.CV;
+using Emgu.Util;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV.Structure;
+
+
+namespace HumanDetectionAndRecognition
+{
+     public class ConnectedComponent
+    {
+        byte _nextLabel = 0;
+        public List<ComponentData> components = new List<ComponentData>();
+
+        /// <summary>
+        /// Find all connected components in image
+        /// </summary>
+        /// <param name="inpImage"> Current frame Binary</param>
+        /// <returns> List of all connected components in the image and
+        /// info about them like Silhouette,top,lower,right and left points</returns>
+        unsafe public List<ComponentData> Find_ConnectedComponent(Image<Gray, Byte> inpImage)
+        {
+            _nextLabel = 0;
+            components.Clear();
+            List<ComponentData> _tempComponent = new List<ComponentData>();
+            inpImage = FirstPath(inpImage.Clone());
+            inpImage = SecondPath(inpImage.Clone());
+            FilleachObjectPar(inpImage.Clone());
+            foreach (ComponentData comp in components)
+            {
+                //if (comp.getHeight > 30 && comp.getWidth > 13 && comp.getWidth < 40)  //50,60
+                //{
+                    comp.Silhouette = ExtractImage(inpImage.Clone(), comp.UpperPoint.X, comp.LowerPoint.X, comp.LeftPoint.Y, comp.RightPoint.Y, comp.Label).Mul(255);
+                    _tempComponent.Add(comp);
+                //}
+            }
+            components = _tempComponent;
+            return components;
+        }
+
+        /// <summary>
+        /// Copy part of Gray image based on top,lower,right and left points and part label
+        /// </summary>
+        /// <param name="originImage">the original image to be copied.</param>
+        /// <param name="XUpper">the upper boundary</param>
+        /// <param name="XLower">the lower boundary</param>
+        /// <param name="YLeft">the left boundary</param>
+        /// <param name="YRight">the right boundary</param>
+        /// <param name="CopyLabel">the copy label</param>
+        /// <returns>copied Part in Gray image </returns>
+        unsafe public static Image<Gray, Byte> ExtractImage(Image<Gray, Byte> originImage, int XUpper, int XLower, int YLeft, int YRight, byte CopyLabel)
+        {
+            try
+            {
+                Image<Gray, Byte> destination = new Image<Gray, byte>(YRight - YLeft, XLower - XUpper);
+                fixed (byte* ptr_src = originImage.Data)
+                {
+                    fixed (byte* ptr_dest = destination.Data)
+                    {
+                        int i_src = 0;
+                        int i_dest = 0;
+                        for (int row = XUpper; row < XLower; row++)
+                        {
+                            i_src = (row * (originImage.Data.Length / originImage.Height)) + YLeft;
+                            for (int col = YLeft; col < YRight; col++)
+                            {
+                                if (*(ptr_src + i_src) == CopyLabel)
+                                    *(ptr_dest + i_dest) = *(ptr_src + i_src);
+                                i_src++;
+                                i_dest++;
+                            }
+                            i_dest += (destination.Data.Length / destination.Height) % destination.Width;
+                        }
+                    }
+                }
+                return destination;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show("Error Done in ExtractImage");
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Do first path of connected component algo. starting from point (0,0)
+        /// </summary>
+        /// <param name="inpImage">current frame</param>
+        /// <returns>gray image each object has special label based on first path </returns>
+        unsafe Image<Gray, Byte> FirstPath(Image<Gray, Byte> inpImage)
+        {
+            try
+            {
+                int i = 0;
+                fixed (byte* ptr_binaryImage = inpImage.Data)
+                {
+                    for (int row = 0; row < inpImage.Height; row++)
+                    {
+                        for (int col = 0; col < inpImage.Data.Length / inpImage.Height; col++)
+                        {
+                            if (*(ptr_binaryImage + i) == (byte)255)
+                            {
+                                byte label = Find8Neighbours(ptr_binaryImage, i, (inpImage.Data.Length / inpImage.Height), inpImage.Data.Length);
+                                if (label == 255)
+                                {
+                                    _nextLabel++;
+                                    *(ptr_binaryImage + i) = _nextLabel;
+                                }
+                                else
+                                    *(ptr_binaryImage + i) = label;
+                            }
+                            i++;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show("Error Done in FirstPath");
+            }
+            return inpImage.Clone();
+        }
+
+        /// <summary>
+        /// Do second path of connected component algo. starting from point (n,n)
+        /// </summary>
+        /// <param name="inpImage">gray image that are resulted from 1st path</param>
+        /// <returns>gray image each connected component has special label based on 2ed path</returns>
+        unsafe Image<Gray, Byte> SecondPath(Image<Gray, Byte> inpImage)
+        {
+            try
+            {
+                fixed (byte* ptr_binaryImage = inpImage.Data)
+                {
+                    int i = 0;
+                    int temp = 0;
+                    for (int row = inpImage.Height - 1; row > -1; row--)
+                    {
+                        temp = (row) * (inpImage.Data.Length / inpImage.Height);
+                        for (int col = (inpImage.Data.Length / inpImage.Height) - 1; col > -1; col--)
+                        {
+                            i = temp + col;
+                            byte v = *(ptr_binaryImage + i);
+                            if (*(ptr_binaryImage + i) != 0)
+                            {
+                                byte label = Find8Neighbours(ptr_binaryImage, i, inpImage.Data.Length / inpImage.Height, inpImage.Data.Length);
+                                if (label != 255)
+                                {
+                                    if (label != *(ptr_binaryImage + i))
+                                        *(ptr_binaryImage + i) = label;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show("Error Done in SecondPath");
+            }
+            return inpImage.Clone();
+        }
+
+        /// <summary>
+        /// Find the label of neighbour pixels
+        /// </summary>
+        /// <param name="ptr">pointer to your image</param>
+        /// <param name="pixel_index">index of current pixel</param>
+        /// <param name="rowlenth">image width</param>
+        /// <param name="imageLength">image length</param>
+        /// <returns>if there were labeled neighbour return min labeled neighbour else return 255</returns>
+        unsafe byte Find8Neighbours(byte* ptr, int pixel_index, int rowlenth, int imageLength)
+        {
+            try
+            {
+                byte label = 255;
+                if (((pixel_index + 1) % rowlenth) != 1)
+                {
+                    if (*((ptr + pixel_index) + rowlenth - 1) != 0)
+                    {
+                        if (label > *((ptr + pixel_index) + rowlenth - 1))
+                            label = *((ptr + pixel_index) + rowlenth - 1);
+                    }
+                    if (*((ptr + pixel_index) - 1) != 0)
+                    {
+                        if (label > *((ptr + pixel_index) - 1))
+                            label = *((ptr + pixel_index) - 1);
+                    }
+                    if (*((ptr + pixel_index) - rowlenth - 1) != 0)
+                    {
+                        if (label > *((ptr + pixel_index) - 1 - rowlenth))
+                            label = *((ptr + pixel_index) - 1 - rowlenth);
+                    }
+                }
+                if ((pixel_index + 1) > rowlenth)
+                {
+                    if (*((ptr + pixel_index) - rowlenth) != 0)
+                    {
+                        if (label > *((ptr + pixel_index) - rowlenth))
+                            label = *((ptr + pixel_index) - rowlenth);
+                    }
+                }
+                if ((pixel_index + 1) % rowlenth != 0)
+                {
+                    if (*((ptr + pixel_index) + rowlenth + 1) != 0)
+                    {
+                        if (label > *((ptr + pixel_index) + rowlenth + 1))
+                            label = *((ptr + pixel_index) + rowlenth + 1);
+                    }
+                    if (*((ptr + pixel_index) + 1) != 0)
+                    {
+                        if (label > *((ptr + pixel_index) + 1))
+                            label = *((ptr + pixel_index) + 1);
+                    }
+                    if (*((ptr + pixel_index) - rowlenth + 1) != 0)
+                    {
+                        if (label > *((ptr + pixel_index) - rowlenth + 1))
+                            label = *((ptr + pixel_index) - rowlenth + 1);
+                    }
+                }
+                if ((pixel_index + 1) < imageLength)
+                {
+                    if (*((ptr + pixel_index) + rowlenth) != 0)
+                    {
+                        if (label > *((ptr + pixel_index) + rowlenth))
+                            label = *((ptr + pixel_index) + rowlenth);
+                    }
+                }
+                return label;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show("Error Done in Find8Neighbours");
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Add connected component to component list that contain
+        /// info about each object not add silhoutte in this fn
+        /// </summary>
+        /// <param name="inpImage">result image come from 2nd path</param>
+        unsafe void FilleachObjectPar(Image<Gray, Byte> inpImage)
+        {
+            try
+            {
+                int i = 0;
+                bool found_pixel = false;
+                fixed (byte* ptr_binaryImage = inpImage.Data)
+                {
+                    for (int row = 0; row < inpImage.Height; row++)
+                        for (int col = 0; col < inpImage.Data.Length / inpImage.Height; col++)
+                        {
+                            if (*(ptr_binaryImage + i) != 0)
+                            {
+                                foreach (ComponentData comp in components)
+                                {
+                                    if (*(ptr_binaryImage + i) == comp.Label)
+                                    {
+                                        if (col < comp.LeftPoint.Y)
+                                            comp.LeftPoint = new Point(row, col);
+                                        if (col > comp.RightPoint.Y)
+                                            comp.RightPoint = new Point(row, col);
+                                        if (row > comp.LowerPoint.X)
+                                            comp.LowerPoint = new Point(row, col);
+                                        found_pixel = true;
+                                        break;
+                                    }
+                                    else
+                                        found_pixel = false;
+                                }
+                                if (!found_pixel)
+                                {
+                                    ComponentData comp = new ComponentData();
+                                    comp.Label = *(ptr_binaryImage + i);
+                                    comp.UpperPoint = new Point(row, col);
+                                    comp.LowerPoint = new Point(row, col);
+                                    comp.RightPoint = new Point(row, col);
+                                    comp.LeftPoint = new Point(row, col);
+                                    components.Add(comp);
+                                }
+                            }
+                            i++;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show("Error Done in FilleachObjectPar");
+            }
+        }
+    }
+}
